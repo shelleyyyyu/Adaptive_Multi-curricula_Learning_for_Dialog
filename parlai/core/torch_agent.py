a2bbb553ea4f450ad65e2ffb78198dfacb766720
shelleyyyyu <menghsuanyu26@gmail.com>
2021-10-22 14:36:39 +0800

    fix loss backward bug

--- parlai/core/torch_agent.py
+++ parlai/core/torch_agent.py
@@ -1501,7 +1501,7 @@ class TorchAgent(Agent):
         if self.fp16:
             self.optimizer.backward(loss, update_master_grads=False)
         else:
-            loss.backward()
+            loss.backward(retain_graph=True)
 
     def update_params(self):
         """
