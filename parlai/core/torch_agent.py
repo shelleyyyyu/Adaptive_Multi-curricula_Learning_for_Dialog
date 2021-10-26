73bafda668249718a3ecada4ca20eb72945f0ce8
shelleyyyyu <menghsuanyu26@gmail.com>
2021-10-26 14:17:00 +0800

    update

--- parlai/core/torch_agent.py
+++ parlai/core/torch_agent.py
@@ -1501,7 +1501,7 @@ class TorchAgent(Agent):
         if self.fp16:
             self.optimizer.backward(loss, update_master_grads=False)
         else:
-            loss.backward(retain_graph=True)
+            loss.backward()
 
     def update_params(self):
         """
