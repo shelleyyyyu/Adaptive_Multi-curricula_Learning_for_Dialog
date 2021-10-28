c6dcc10adc60814c4ba53ac5a6f51e6d98707690
shelleyyyyu <menghsuanyu26@gmail.com>
2021-10-28 20:16:55 +0800

    remove sum and mean use f to calculate cosine similaroty

--- parlai/agents/adaptive_learning/seq2seq.py
+++ parlai/agents/adaptive_learning/seq2seq.py
@@ -189,10 +189,10 @@ class AdaSeq2seqAgent(Seq2seqAgent):
         cur_batch_input_emb = self.model.pretrain_embedding(cur_batch_input[0])
         if self.prev_batch_input is not None and len(batch.text_vec) == self.opt['batchsize']:
             prev_batch_input_emb = self.model.pretrain_embedding(self.prev_batch_input)
-            prev_batch_input_emb_mean = torch.sum(torch.mean(prev_batch_input_emb, 0),0)
-            cur_batch_input_emb_mean = torch.sum(torch.mean(cur_batch_input_emb, 0),0)
-            margin_loss = -1 * distance.cosine(prev_batch_input_emb_mean.cpu(), cur_batch_input_emb_mean.cpu())
-            # margin_loss = -F.cosine_similarity(prev_batch_input_emb, cur_batch_input_emb).abs().mean()
+            # prev_batch_input_emb_mean = torch.sum(torch.mean(prev_batch_input_emb, 0),0)
+            # cur_batch_input_emb_mean = torch.sum(torch.mean(cur_batch_input_emb, 0),0)
+            # margin_loss = -1 * distance.cosine(prev_batch_input_emb_mean.cpu(), cur_batch_input_emb_mean.cpu())
+            margin_loss = -F.cosine_similarity(prev_batch_input_emb, cur_batch_input_emb).abs().mean()
             loss = self.margin_rate * margin_loss + (1 - self.margin_rate) * generation_loss
         else:
             # loss = generation_loss
