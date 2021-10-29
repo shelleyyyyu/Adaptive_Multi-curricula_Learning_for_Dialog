d3e9b35ab13d4aa4087a9b030de3f9c6d1e3ecd3
shelleyyyyu <menghsuanyu26@gmail.com>
2021-10-29 14:49:29 +0800

    update args

--- projects/adaptive_learning/train.py
+++ projects/adaptive_learning/train.py
@@ -38,9 +38,9 @@ OVERRIDE = {
     "num_epochs": 20,
     "display_examples": False,
     "history_size": -1,
-    "text_truncate": 64,
-    "label_truncate": 64,
-    "truncate": 64,
+    "text_truncate": 48,
+    "label_truncate": 48,
+    "truncate": 48,
     "gpu": 0,
     "batch_sort_field": 'label',
     "pytorch_teacher_batch_sort": False,
@@ -51,7 +51,7 @@ OVERRIDE = {
     "beam_size": 1,
     "margin_rate": 0.01,
     "margin": 0.5,
-    "fix_pad_length":64
+    "fix_pad_length":48
 }
 
 if __name__ == '__main__':
@@ -93,9 +93,9 @@ if __name__ == '__main__':
         save_after_valid=False,
         margin_rate=0.5,
         margin=0.5,
-        fix_pad_length=64,
+        fix_pad_length=48,
         policy_n_heads=6,
-        policy_n_layers=3,
+        policy_n_layers=2,
         policy_ffn_size=128,
         policy_embedding_size=300,
         policy_attention_dropout=0.1,
