8be8594d3c7e6a9e340914a36554d16d316967c6
shelleyyyyu <menghsuanyu26@gmail.com>
2021-11-02 14:57:31 +0800

    fix bug

--- parlai/tasks/adaptive_learning/agents.py
+++ parlai/tasks/adaptive_learning/agents.py
@@ -499,8 +499,6 @@ class DefaultTeacher(FbDialogTeacher):
             if not self.random_policy:
                 with torch.no_grad():
                     current_states, margin_loss, mean_input_embed = self._build_states(observations)
-                # print('__load_training_batch mean_input_embed', mean_input_embed)
-                # print()
                 self.history_mean_embed.append(mean_input_embed.detach())
                 history_mean_emb_tensor = torch.stack(self.history_mean_embed[:10], dim=0)
                 action_probs = self.policy(current_states, history_mean_emb_tensor)#torch.unsqueeze(mean_input_embed.detach(), 0))#history_mean_emb_tensor)
